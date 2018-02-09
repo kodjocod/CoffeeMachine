f93f4ebd3df796063d3da83433183370f17a022e
kkodjo <kodjocod@gmail.com>
2018-02-09 18:05:11 +0100

    premier commit pour exo2

--- ExoCoffeeMachine/CoffeeMachineFirstIteration/CoffeeMachineIterations/Program.cs
+++ ExoCoffeeMachine/CoffeeMachineFirstIteration/CoffeeMachineIterations/Program.cs
@@ -11,9 +11,8 @@ namespace CoffeeMachine
         static void Main(string[] args)
         {
             Console.WriteLine("Démarrage des commandes");
-            Console.WriteLine("Veuillez entrer votre commande sous le format : X:X:X");
+            Console.WriteLine("Veuillez entrer votre commande sous le format :Boisson:Sucre:Touillettes:Prix "); //exemple "T:1:0:0,5";
             string commande = Console.ReadLine();
-    
             string[] strCommande = null;
             char[] splitdelimiter = { ':' };
             strCommande= commande.Split(splitdelimiter);
@@ -21,32 +20,47 @@ namespace CoffeeMachine
 
             int nbsugar;
             int nbstick;
+      
             bool isSuccessul = Int32.TryParse(strCommande[1], out nbsugar);
-            if (!isSuccessul) 
+            if (!isSuccessul)
                 nbsugar = 0;
 
             isSuccessul = Int32.TryParse(strCommande[2], out nbstick);
             if (!isSuccessul)
                 nbstick = 0;
 
-            if (nbsugar >= 1 && nbstick==0)
+            if (nbsugar >= 1 && nbstick == 0)
                 nbstick += 1;
+
+            double prixsaisie = Double.Parse(strCommande[3]);
             
 
             switch (boissonchoisi)
             {
                 case "T":
-                    Tea tea = new Tea(nbsugar, nbstick);
-                    Console.WriteLine(tea.EnvoyerMessage());
+                    Tea tea = new Tea(nbsugar, nbstick, prixsaisie);
+                    if (prixsaisie < tea.Price)
+                    {
+                        Console.WriteLine("Le montant saisie est inférieure au montant minimal il manque :{0} euros", tea.Price - prixsaisie);
+                    }
+                    else if (prixsaisie> tea.Price)
+                    {
+                        Console.WriteLine("Votre monnaie est :{0} euros", prixsaisie -  tea.Price );
+                        Console.WriteLine(tea.EnvoyerMessage());
+                    }
+                    else
+                    {
+
+                    }
                     break;
                 case "H":
-                    Chocolate choco = new Chocolate(nbsugar, nbstick);
-                    Console.WriteLine(choco.EnvoyerMessage());
+                    //Chocolate choco = new Chocolate(nbsugar, nbstick);
+                  //  Console.WriteLine(choco.EnvoyerMessage());
                     break;
                 case "C":
 
-                    Coffee coffee = new Coffee(nbsugar , nbstick);
-                    Console.WriteLine(coffee.EnvoyerMessage());
+                  //  Coffee coffee = new Coffee(nbsugar , nbstick);
+                   // Console.WriteLine(coffee.EnvoyerMessage());
                     break;
             }
             Console.ReadKey();
@@ -56,8 +70,10 @@ namespace CoffeeMachine
 
    public class Drink
     {
-        public int NbSugar { get; set; }
-        public int NbStick { get; set; }
+        public int NbSugar { get;  set; }
+        public int NbStick { get;  set; }
+ 
+
 
         public Drink( int nbsugar, int nbStick)
         {
@@ -74,10 +90,30 @@ namespace CoffeeMachine
     }
    public  class Tea : Drink
     {
-        public Tea( int nbsugar, int nbStick) : base(nbsugar, nbStick)
+        private double price=0.4;
+        public double Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                if ((value >= 0.4))
+                {
+                    price = value;
+                }
+
+            }
+
+        }
+
+
+        public Tea( int nbsugar, int nbStick, double price) : base(nbsugar, nbStick)
         {
             NbSugar = nbsugar;
             NbStick = nbStick;
+            Price = price;
         }
 
 
@@ -90,11 +126,30 @@ namespace CoffeeMachine
     }
    public class Coffee : Drink
     {
-        public Coffee( int nbsugar, int nbStick) : base( nbsugar, nbStick)
+        private double price = 0.6;
+        public double Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                if ((value >= 0.6))
+                {
+                    price = value;
+                }
+
+            }
+
+        }
+
+        public Coffee( int nbsugar, int nbStick,double price) : base( nbsugar, nbStick)
         {
 
             NbSugar = nbsugar;
             NbStick = nbStick;
+            Price = price;
         }
         public override string EnvoyerMessage()
         {
@@ -103,10 +158,29 @@ namespace CoffeeMachine
     }
    public  class Chocolate : Drink
     {
-        public Chocolate(int nbsugar, int nbStick) : base( nbsugar, nbStick)
+        private double price = 0.5;
+        public double Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                if ((value >= 0.5))
+                {
+                    price = value;
+                }
+
+            }
+
+        }
+
+        public Chocolate(int nbsugar, int nbStick,double price) : base( nbsugar, nbStick)
         {
             NbSugar = nbsugar;
             NbStick = nbStick;
+            Price = price;
         }
         public override string EnvoyerMessage()
         {
--- ExoCoffeeMachine/CoffeeMachineFirstIteration/UnitTestCoffeeMachineIterations/UnitTest1.cs
+++ ExoCoffeeMachine/CoffeeMachineFirstIteration/UnitTestCoffeeMachineIterations/UnitTest1.cs
@@ -7,24 +7,39 @@ namespace UnitTestExoCoffeeMachine
     [TestClass]
     public class UnitTestCoffeeMachine
     {
-        [TestMethod]
-        public void CommandeCoffeWith2Sugars()
+        //[TestMethod]
+        //public void CommandeCoffeWith2Sugars()
+        //{
+        //    string commande ="C:2:0";
+        //    string[] strCommande = null;
+        //    char[] splitdelimiter = { ':' };
+        //    strCommande = commande.Split(splitdelimiter);
+        //    int nbsugar = Int32.Parse(strCommande[1]);
+        //    int nbstick= Int32.Parse(strCommande[2]);
+        //    if (nbsugar >= 1 && nbstick == 0)
+        //        nbstick += 1;
+        //    Coffee coffee = new Coffee(nbsugar, nbstick);
+        //    Assert.AreEqual("Drink maker makes coffee with 2 sugar(s) and 1 stick(s)",coffee.EnvoyerMessage());
+        //}
+        //[TestMethod]
+        //public void CommandeTeaWith1Stick()
+        //{
+        //    string commande = "T:1:0";
+        //    string[] strCommande = null;
+        //    char[] splitdelimiter = { ':' };
+        //    strCommande = commande.Split(splitdelimiter);
+        //    int nbsugar = Int32.Parse(strCommande[1]);
+        //    int nbstick = Int32.Parse(strCommande[2]);
+        //    if (nbsugar >= 1 && nbstick == 0)
+        //        nbstick += 1;
+        //    Tea tea = new Tea (nbsugar, nbstick);
+        //    Assert.AreEqual("Drink maker makes tea with 1 sugar(s) and 1 stick(s)", tea.EnvoyerMessage());
+
+        //}
+        [TestMethod] 
+        public void CommandTeaWithMinimumPrice()
         {
-            string commande ="C:2:0";
-            string[] strCommande = null;
-            char[] splitdelimiter = { ':' };
-            strCommande = commande.Split(splitdelimiter);
-            int nbsugar = Int32.Parse(strCommande[1]);
-            int nbstick= Int32.Parse(strCommande[2]);
-            if (nbsugar >= 1 && nbstick == 0)
-                nbstick += 1;
-            Coffee coffee = new Coffee(nbsugar, nbstick);
-            Assert.AreEqual("Drink maker makes coffee with 2 sugar(s) and 1 stick(s)",coffee.EnvoyerMessage());
-        }
-        [TestMethod]
-        public void CommandeTeaWith1Stick()
-        {
-            string commande = "T:1:0";
+            string commande = "T:1:0:0.5";
             string[] strCommande = null;
             char[] splitdelimiter = { ':' };
             strCommande = commande.Split(splitdelimiter);
@@ -32,8 +47,9 @@ namespace UnitTestExoCoffeeMachine
             int nbstick = Int32.Parse(strCommande[2]);
             if (nbsugar >= 1 && nbstick == 0)
                 nbstick += 1;
-            Tea tea = new Tea (nbsugar, nbstick);
+            Tea tea = new Tea(nbsugar, nbstick);
             Assert.AreEqual("Drink maker makes tea with 1 sugar(s) and 1 stick(s)", tea.EnvoyerMessage());
+
 
         }
 
